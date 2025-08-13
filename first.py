ebb591c46965db867ab31654405e4a2ff22b0422
dhvanit2804 <dhvanitparate123@gmail.com>
2025-08-13 21:56:48 +0530

    13-08-2025

--- first.py
+++ first.py
@@ -1,6 +1,24 @@
-age = int(input("Enter your age: "))
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
 
-if age >= 18:
-    print("You are eligible to vote.")
-else:
-    print("You are not eligible to vote")
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.
+
+Then the trav'ller in the dark,
+Thanks you for your tiny spark,
+He could not see which way to go,
+If you did not twinkle so.
+
+In the dark blue sky you keep,
+And often thro' my curtains peep,
+For you never shut your eye,
+Till the sun is in the sky.
+
+'Tis your bright and tiny spark,
+Lights the trav'ller in the dark:
+Tho' I know not what you are,
+Twinkle, twinkle, little star.''')
---
+++ module.py
@@ -0,0 +1,6 @@
+import pyjokes
+
+# print("Printing Jokes...")
+# This is Random Joke
+joke = pyjokes.get_joke()
+print(joke)
