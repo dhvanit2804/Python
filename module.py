ad61d9a312f0623582ac442263b3a6c8a2825b9e
dhvanit2804 <dhvanitparate123@gmail.com>
2025-08-14 12:13:37 +0530

    14-08-2025

--- module.py
+++ module.py
@@ -1,6 +1,11 @@
-import pyjokes
+# import pyjokes
 
-# print("Printing Jokes...")
-# This is Random Joke
-joke = pyjokes.get_joke()
-print(joke)
+# # print("Printing Jokes...")
+# # This is Random Joke
+# joke = pyjokes.get_joke()
+# print(joke)
+import pyttsx3
+
+engine = pyttsx3.init()
+engine.say("Meet Is Fucking Bitch...")
+engine.runAndWait()
--- variable.py
+++ variable.py
@@ -1,7 +1,10 @@
-name = "Dhvanit"
-age = 25
-height = 5.9
+age = 20 # Integer
+name = "Dhvanit" #String
+cgpa = 4.55 # float
+is_Complete = True # Can Also be False
 
-print(f"My name is {name}, my age is {age} and my height is {height}.")
+print(name)
 print(age)
-print(height)
+print(cgpa)
+print(f"My name is {name}, my age is {age} and my CGPA is {cgpa}.")
+print(is_Complete)
