a1fe227a35fcff400ec8cf3df73534495aeff459
dhvanit2804 <dhvanitparate123@gmail.com>
2025-08-18 18:13:43 +0530

    18-08-2025

---
+++ Chapter 3 Strings/escape_seq.py
@@ -0,0 +1,3 @@
+a = "Dhvanit is a good boy\nbut not a bad boy"
+
+print(a)
---
+++ Chapter 3 Strings/str_function.py
@@ -0,0 +1,13 @@
+name = "Dhvanit"
+
+name1 = "sahil patel"
+
+print(len(name))
+print(name.endswith("nit"))
+print(name.startswith("Dh"))
+
+print(name.capitalize()) # Capitalize First Letter Of the String
+
+print(name1.title()) # Capitalize First letter of each word in string
+
+print(name1.replace("patel", "Dobariya"))
