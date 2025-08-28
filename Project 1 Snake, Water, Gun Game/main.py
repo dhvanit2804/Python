2a4593c55ae29c7951430f7e84596ab7b51274ba
dhvanit2804 <dhvanitparate123@gmail.com>
2025-08-28 21:16:58 +0530

    28-8-2025

--- Project 1 Snake, Water, Gun Game/main.py
+++ Project 1 Snake, Water, Gun Game/main.py
@@ -4,3 +4,42 @@ import random
 -1 for water
 0 for gun
 '''
+
+computer = random.choice([-1, 0, 1])
+youstr = input("Enter your choice: ")
+youDict = {"s": 1, "w": -1, "g": 0}
+reversedict = {1: "Snake", -1: "Water", 0: "Gun"}
+
+if youstr not in youDict:
+    print("Invalid choice! Please enter only 's', 'w', or 'g'.")
+    exit()
+    
+you = youDict[youstr]
+
+print(f"You chose {reversedict[you]}\nComputer chose {reversedict[computer]}")
+
+
+if(computer == you):
+     print("Its a draw")
+
+else:
+    if(computer == -1 and you == 1):
+        print("You win!")
+
+    elif(computer == -1 and you == 0):
+        print("You Lose!")
+
+    elif(computer == 1 and you == -1):
+        print("You Lose!")
+
+    elif(computer == 1 and you == 0):
+        print("You win!")
+
+    elif(computer ==0 and you == -1):
+            print("You Win!")
+
+    elif(computer == 0 and you == 1):
+            print("You Lose!")
+
+    else:
+        print("Something went wrong!")
