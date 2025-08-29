3a1ffd8982674be71ea48293a26367ca74892162
dhvanit2804 <dhvanitparate123@gmail.com>
2025-08-29 16:42:01 +0530

    28-8-2025

---
+++ Chapter 9 - PS/06_problem6.py
@@ -0,0 +1,9 @@
+'Write a program to mine a log file and find out whether it contains "python".'
+
+with open("log.txt") as f:
+    content = f.read()
+
+if("python" in content):
+    print("python is present")
+else:
+    print("python is not present")
