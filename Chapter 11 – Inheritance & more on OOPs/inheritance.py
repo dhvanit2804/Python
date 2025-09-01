6e0f8ecb74428f50e8666a072ab9276cce805dee
dhvanit2804 <dhvanitparate123@gmail.com>
2025-09-01 16:06:43 +0530

    1-9-2025

---
+++ Chapter 11 – Inheritance & more on OOPs/inheritance.py
@@ -0,0 +1,11 @@
+'What is Inheritance?'
+'Inheritance is a mechanism in OOP that allows a class to inherit attributes and methods from another class.'
+'Inheritance is a way of creating a new class from an existing class.'
+
+class Employee:
+    def show(self):
+        print(f"The name is {self.name} and the salary is {self.salary}")
+
+class Programmer:
+    def show(self):
+        print(f"The name is {self.name} and the salary is {self.salary}")
