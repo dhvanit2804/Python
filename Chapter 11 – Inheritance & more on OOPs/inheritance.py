105021fbcf1bfc791731e5c64f9e98e925d6013e
dhvanit2804 <dhvanitparate123@gmail.com>
2025-09-05 10:25:54 +0530

    5-9-2025

--- Chapter 11 – Inheritance & more on OOPs/inheritance.py
+++ Chapter 11 – Inheritance & more on OOPs/inheritance.py
@@ -3,9 +3,24 @@
 'Inheritance is a way of creating a new class from an existing class.'
 
 class Employee:
+    company = "ITC"
     def show(self):
-        print(f"The name is {self.name} and the salary is {self.salary}")
+        print(f"The name of the Employee is {self.name} and the salary is {self.salary}")
 
-class Programmer:
-    def show(self):
-        print(f"The name is {self.name} and the salary is {self.salary}")
+# class Programmer:
+#     company = "ITC infotech"
+#     def show(self):
+#         print(f"The name is {self.name} and the salary is {self.salary}")
+
+#     def showlanguage(self):
+#         print(f"The name is {self.name} he is good in {self.language} language")
+
+class Programmer(Employee):
+    company = "ITC infotech"
+    def showlanguage(self):
+        print(f"The name is {self.name} he is good in {self.language} language")
+
+a = Employee()
+b = Programmer()
+
+print(a.company, b.company)
