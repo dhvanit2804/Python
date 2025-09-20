c878aaad8acfa502312697faed6b649b35abee25
dhvanit2804 <dhvanitparate123@gmail.com>
2025-09-20 17:17:16 +0530

    20-9-2025

--- Chapter 11 - PS/04_problem4.py
+++ Chapter 11 - PS/04_problem4.py
@@ -8,6 +8,11 @@ class Complex:
 
     def __add__(self, c2):
         return Complex(self.r + c2.r, self.i + c2.i)
+    
+    def __mul__(self, c2):
+        real = self.r * c2.r - self.i * c2.i
+        imag = self.r * c2.i + self.i * c2.r
+        return Complex(real, imag)
 
     def __str__(self):
         return f"{self.r} + {self.i}i"
@@ -15,3 +20,4 @@ class Complex:
 c1 = Complex(1, 2)
 c2 = Complex(3, 4)
 print(c1 + c2)
+print(c1 * c2)
