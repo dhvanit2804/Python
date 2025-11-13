dca17bc872992561c4ad33447860415b89f4efac
dhvanit <dhvanitparate123@gmail.com>
2025-11-13 21:27:01 +0530

    13-11-2025

--- Mega Project 1 - Jarvis/main.py
+++ Mega Project 1 - Jarvis/main.py
@@ -1,39 +1,54 @@
 import speech_recognition as sr
 import webbrowser
 import pyttsx3
+import musicLibrary
 
 recognizer = sr.Recognizer()
 engine = pyttsx3.init()
 
 def speak(text):
+    engine = pyttsx3.init()
     engine.say(text)
     engine.runAndWait()
+    engine.stop()
 
 def processCommand(c):
-    print(c)
-    pass
+    c = c.lower()
+    if "open google" in c:
+        webbrowser.open("https://google.com")
+    elif "open facebook" in c:
+        webbrowser.open("https://facebook.com")
+    elif "open instagram" in c:
+        webbrowser.open("https://instagram.com")
+    elif "open youtube" in c:
+        webbrowser.open("https://youtube.com")
+    elif "open github" in c:
+        webbrowser.open("https://github.com/dhvanit2804")
+    elif c.lower().startswith("play"):
+        song = c.lower().split(" ")[1]
+        link = musicLibrary.music[song]
+        webbrowser.open(link)
 
 if __name__ == "__main__":
     speak("Initializing Jarvis......")
     while True:
-        # Listen for the wake word "Jarvis"
-        
-        print("recognizing...")
+        print("Recognizing...")
         try:
             with sr.Microphone() as source:
                 print("Listening...")
-                audio = recognizer.listen(source, timeout=3, phrase_time_limit=1)
+                audio = recognizer.listen(source, timeout=3, phrase_time_limit=2)
             word = recognizer.recognize_google(audio)
-            if(word.lower() == "jarvis"):
-                speak("Ya")
-                # Listen for command
+            print(f"Heard: {word}")
+
+            if "jarvis" in word.lower():
+                speak("Yaa")
+
                 with sr.Microphone() as source:
                     print("Jarvis Active...")
                     audio = recognizer.listen(source)
                     command = recognizer.recognize_google(audio)
 
-                    processCommand("Hi")
-
+                    processCommand(command)
 
         except Exception as e:
-            print("Error; {0}".format(e))
+            print("Error:", e)
---
+++ Mega Project 1 - Jarvis/musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "ishq": "https://youtu.be/CmbsfW0Ckb4?si=ALjYGdJHb31rBusL",
+    "panvadi": "https://youtu.be/usVq_4K_m_w?si=f-8pAPrb-qyplqvB",
+    "soft": "https://youtu.be/cWMxCE2HTag?si=tzIxUCijpHOiTkjo,",
+    "onelove": "https://youtu.be/0pWsCiBvLOk?si=ByIBywOIbaKr5YfC"
+}
